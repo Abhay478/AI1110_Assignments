78dadf39fb5b76b8c72479ea7867def2d7fe5c28
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-24 14:22:38 +0530

    Add files via upload

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -13,9 +13,11 @@ for k in freq:
         lst.append(j)
     i += 5
 
-n, bins, patches = plt.hist(x = np.array(lst), bins=9, alpha=0.7, rwidth=0.85, range=(31, 75))
-plt.grid(axis='y')
-plt.annotate("([46, 50], 3)", (46, 3))
+n, bins, patches = plt.hist(x = np.array(lst), bins=9, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
+plt.grid(axis='y', alpha = 0.3)
+plt.xlabel("Weights in kg")
+plt.ylabel("Number of students")
+plt.annotate("([46, 50], 3)", (46, 3.25))
 plt.xticks([5 * (i + 6) + 1 for i in range(9)])
 
 plt.show()
