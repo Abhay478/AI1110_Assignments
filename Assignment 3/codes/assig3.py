4206c7310dd3f36abfac1a8839dab58fbc8b1620
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-26 23:17:17 +0530

    Add files via upload

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -2,33 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-lst = np.array([])
+
 x = []
-read = pd.read_excel('./assig3.xlsx')
+read = pd.read_excel('/Users/abhay/Latex/tables/assig3.xlsx')
 raw_data = np.array(read)
 freq = [row[1] for row in raw_data]
 
-#freq.append(row[1] for row in raw_data)
-i = 31
 bin = [5 * (i + 6) + 1 for i in range(10)]
-print(freq)
 
 x = [np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)]
-#print(x)
-count = 0
-
-while count < len(x):
-    lst = np.concatenate((lst, np.array(x[count])))
-    count += 1
-    
-    
-        
-
-#print(lst)
 
 
+def merge(x):
+    lst = np.array([])
+    count = 0
+    while count < len(x):
+        lst = np.concatenate((lst, np.array(x[count])))
+        count += 1
+    return lst
+  
+  
+lst = merge(x)
 
-n, bins, patches = plt.hist(x=lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
+plt.hist(x=lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
 
 plt.grid(axis='y', alpha = 0.3)
 plt.xlabel("Weights in kg")
