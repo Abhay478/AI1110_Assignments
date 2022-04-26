c42d34211879fa9b7e79a387c902beaa56c868bd
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-26 23:12:34 +0530

    Add files via upload

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -2,30 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-lst = []
+lst = np.array([])
 x = []
-read = pd.read_excel('./tables/assig3.xlsx')
+read = pd.read_excel('/Users/abhay/Latex/tables/assig3.xlsx')
 raw_data = np.array(read)
-freq = []
-for row in raw_data:
-    freq.append(row[1])
+freq = [row[1] for row in raw_data]
+
+#freq.append(row[1] for row in raw_data)
 i = 31
-for k in freq:
-    x = np.linspace(i, i + 4, k)
-    
-    for j in x:
-        lst.append(j)
-    i += 5
-
-
 bin = [5 * (i + 6) + 1 for i in range(10)]
+print(freq)
 
-n, bins, patches = plt.hist(x = np.array(lst), bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
+x = [np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)]
+#print(x)
+count = 0
+
+while count < len(x):
+    lst = np.concatenate((lst, np.array(x[count])))
+    count += 1
+    
+    
+        
+
+#print(lst)
+
+
+
+n, bins, patches = plt.hist(x=lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
 
 plt.grid(axis='y', alpha = 0.3)
 plt.xlabel("Weights in kg")
 plt.ylabel("Number of students")
 plt.annotate("([46, 50], 3)", (46, 3.25))
-plt.xticks([5 * (i + 6) + 1 for i in range(10)])
+plt.xticks(bin)
 
 plt.show()
+
+
