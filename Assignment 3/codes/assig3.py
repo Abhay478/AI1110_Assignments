2b9c4732eb65a7aa5077509381622df5a3596208
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-27 09:55:16 +0530

    Add files via upload

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -12,19 +12,12 @@ bin = [5 * (i + 6) + 1 for i in range(10)]
 
 x = [np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)]
 
+lst = [np.concatenate(tuple(x))]
 
-def merge(x):
-    lst = np.array([])
-    count = 0
-    while count < len(x):
-        lst = np.concatenate((lst, np.array(x[count])))
-        count += 1
-    return lst
-  
-  
-lst = merge(x)
+print(lst)
 
-plt.hist(x=lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
+
+plt.hist(x=lst[0], bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
 
 plt.grid(axis='y', alpha = 0.3)
 plt.xlabel("Weights in kg")
