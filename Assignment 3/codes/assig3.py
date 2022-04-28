c4261f23770aed357e68c1a49d6a59da3ddfce6c
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-28 09:55:48 +0530

    Add files via upload

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -2,17 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
-x = []
-read = pd.read_excel('.tables/assig3.xlsx')
+read = pd.read_excel('/Users/abhay/Latex/tables/assig3.xlsx')
 raw_data = np.array(read)
-freq = [row[1] for row in raw_data]
+freq = raw_data[1][1:]
 
-bin = [5 * (i + 6) + 1 for i in range(10)]
+bin = np.array([31, 36, 41, 46, 51, 56, 61, 66, 71, 76])
+#alternatively, bin = np.array([5 * (i + 6) + 1 for i in range(9)]). Manual entry vs. Generator, either one will do.
 
-x = [np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)]
+lst = np.concatenate(tuple(np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)))
+#alternatives include using an actual loop, or entering 38 elements manually. Above approach clearly preferable.
 
-lst = np.concatenate(tuple(x))
 
 plt.hist(lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
 
