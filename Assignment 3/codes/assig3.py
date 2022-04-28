1f2d5540284c623af09d224fbe92a8be57aaffb7
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-28 10:40:32 +0530

    Update assig3.py

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -12,7 +12,7 @@ bin = np.array(raw_data[0][1:])
 #alternatively, bin = np.array([5 * (i + 6) + 1 for i in range(9)]). Excel read vs. Generator, excel read preferable.
 
 lst = np.concatenate(tuple(np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)))
-#alternatives include using an actual loop, or entering 38 elements manually. Above approach clearly preferable.
+#alternatives include using an actual loop block, or entering 38 elements manually. Above approach clearly preferable.
 
 plt.hist(lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
 
