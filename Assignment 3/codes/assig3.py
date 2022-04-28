eb7e480669063ae834dfbd18728e0834d80c9b11
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-04-28 10:32:52 +0530

    Add files via upload

--- Assignment 3/codes/assig3.py
+++ Assignment 3/codes/assig3.py
@@ -4,14 +4,15 @@ import pandas as pd
 
 read = pd.read_excel('/Users/abhay/Latex/tables/assig3.xlsx')
 raw_data = np.array(read)
-freq = raw_data[1][1:]
+freq = np.array(raw_data[1][1:])
 
-bin = np.array([31, 36, 41, 46, 51, 56, 61, 66, 71, 76])
-#alternatively, bin = np.array([5 * (i + 6) + 1 for i in range(9)]). Manual entry vs. Generator, either one will do.
+read = pd.read_excel('/Users/abhay/Latex/tables/rand_var.xlsx')
+raw_data = np.array(read)
+bin = np.array(raw_data[0][1:])
+#alternatively, bin = np.array([5 * (i + 6) + 1 for i in range(9)]). Excel read vs. Generator, excel read preferable.
 
 lst = np.concatenate(tuple(np.linspace(bin[i], bin[i + 1] - 1, freq[i]) for i in range(9)))
 #alternatives include using an actual loop, or entering 38 elements manually. Above approach clearly preferable.
-
 
 plt.hist(lst, bins=bin, alpha=0.7, rwidth=0.85, color = 'brown', range=(31, 75))
 
@@ -19,7 +20,7 @@ plt.grid(axis='y', alpha = 0.3)
 plt.xlabel("Weights in kg")
 plt.ylabel("Number of students")
 plt.annotate("([46, 50], 3)", (46, 3.25))
-plt.xticks(bin)
+plt.xticks(list(bin))
 
 plt.show()
 
