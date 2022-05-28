3f6161b04d3f6f25616ea5f95adab6189012b330
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-05-28 16:30:10 +0530

    Add files via upload

---
+++ Assignment 9/code/assig9.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats import norm
+from scipy import integrate as teg
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(1, 1)
+x = np.linspace(-1, 1, 100)
+
+rv = norm(0, 0.3)
+i = rv.pdf(x) + rv.pdf(-x)
+ii = 1 + 4 * np.exp(x) * rv.pdf(x)
+
+n = teg.simpson(rv.pdf(x), x)
+n1 = teg.simpson(i, x)
+n2 = teg.simpson(ii, x)
+
+ax.plot(x, i / n1, label ='f(0, 0.3)')
+
+ax.plot(x, (ii/n2), label = '(1 + 4exp(x) * f(0, 0.3))*0.1618')
+
+
+plt.legend()
+plt.show()
+
+
+
+
+
